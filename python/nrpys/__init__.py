9f172383c5ce04d0bc08797479da6c2abdefb164
Kai Blin <kblin@biosustain.dtu.dk>
2023-02-23 20:00:18 +0100

    chore: manually create the __init__.py file to avoid a star import that broke linting
    Signed-off-by: Kai Blin <kblin@biosustain.dtu.dk>

---
+++ python/nrpys/__init__.py
@@ -0,0 +1,19 @@
+from .nrpys import (
+    ADomain,
+    Config,
+    PredictionCategory,
+    Prediction,
+    StachPrediction,
+    run,
+)
+
+__doc__ = nrpys.__doc__
+
+__all__ = [
+    "ADomain",
+    "Config",
+    "PredictionCategory",
+    "Prediction",
+    "StachPrediction",
+    "run",
+]
